8fbd2d04d7a2f1cac725fa5f345abbb84817063a
Antonio Mancera <antoniomanuelmancerahevilla@gmail.com>
2021-02-04 16:15:21 +0100

    HE añadido botones

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,11 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+
+interface Componente{
+  icon:string;
+  name:string;
+  redirectTo:string;
+}
+
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
+
+
+  componentes:any[]=[
+    {
+      icon:'american-football-outline',
+      name:'Action Sheet',
+      redirectTo:'/action-sheet'
+    },
+    {
+      icon:'alert-circle-outline',
+      name:'Alert',
+      redirectTo:'/alert'
+    },
+    {
+      icon:'beaker-appstore',
+      name:'Avatar',
+      redirectTo:'/avatar'
+    }
+  ];
+
 
   constructor() { }
 
